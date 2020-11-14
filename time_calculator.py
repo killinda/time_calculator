55f5a92b6148904d2e0d39cfdf698eaf955d78f9
killinda <45329203+killinda@users.noreply.github.com>
2020-11-14 22:58:20 +1100

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,34 +1,25 @@
 def add_time(start, duration,the_day_of_week=None):
   ##Convert to minute
-  clock = start[-2:]
   time = start[:-3]
-
+  clock = start[-2:]
   start_hour = int(time.split(":")[0])
   start_minute = int(time.split(":")[1])
-  if clock == "AM":
-      start_time_as_minute = 60*start_hour + start_minute
-  else:
-      start_time_as_minute = 60*(start_hour+12) + start_minute
   duration_hour = int(duration.split(":")[0])
   duration_minute = int(duration.split(":")[1])
-  duration_time_as_minute = duration_hour*60 + duration_minute
-  ##calculate the duration days
-  end_time_as_minute = start_time_as_minute + duration_time_as_minute
-  if end_time_as_minute>(60*24):
-      duration_day = -(-(end_time_as_minute- (60*24))//(60*24))
-      new_time_hour = (int((end_time_as_minute- (60*24))/60))%24
-      new_time_minute = (end_time_as_minute- (60*24))%60
-      new_time_minute = str(new_time_minute).zfill(2)
+  ##generate the time and duration days
+  if clock == "AM":
+      new_time_hour = start_hour%12 + duration_hour   
   else:
-      new_time_hour = (int(end_time_as_minute/60))%24
-      new_time_minute = (end_time_as_minute)%60
-      duration_day = 0
-      new_time_minute = str(new_time_minute).zfill(2)
-  
-  ##print result according to the requiremets
-  ## Time reqruiemnts
+      new_time_hour = (start_hour%12+12) + duration_hour 
+  new_time_minute = start_minute + duration_minute
+  if new_time_minute > 60 :
+      new_time_minute -=60
+      new_time_hour +=1
+  duration_day = new_time_hour//24
+  new_time_hour %=24
+  new_time_minute = str(new_time_minute).zfill(2)
   if new_time_hour == 0:
-    new_time ="12:"+ new_time_minute + " "+"AM"
+      new_time ="12:"+ new_time_minute + " "+"AM"
   else:
     if new_time_hour < 12:
         new_time = str(new_time_hour)+":"+ new_time_minute + " "+"AM"
@@ -37,11 +28,6 @@ def add_time(start, duration,the_day_of_week=None):
             new_time = str(new_time_hour) + ":" + new_time_minute + " "+"PM"
         else:
             new_time =  str(new_time_hour-12)+ ":" + new_time_minute + " "+"PM"
-  ## Day requriments
-  if duration_day == 1:
-      new_time = new_time + " " + "(next day)"
-  if duration_day >1:
-      new_time = new_time + " "+ "("+ str(duration_day) + " " + "days later)"
   ##calculate the final day of week
   if the_day_of_week :
       the_day_of_week = the_day_of_week.lower()
@@ -50,11 +36,13 @@ def add_time(start, duration,the_day_of_week=None):
       final_day_of_week_number = (week_number + duration_day)%7
       final_day_of_week = Week_name[final_day_of_week_number].capitalize()
       final_day_of_week = ", "+final_day_of_week
-    ##insert final day of week to new_time
-      new_time_list = list(new_time)
-      position = new_time_list.index("M")+1
-      new_time_list.insert(position,final_day_of_week)
-      new_time = "".join(new_time_list)
+      new_time +=final_day_of_week 
+  ##generate final settting
+  if duration_day == 1:
+      new_time +=" (next day)"
+  elif duration_day > 1:
+      new_time +=" "+"(" + str(duration_day) + " days later)"
+
   return new_time
 
 
